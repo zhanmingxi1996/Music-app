dc8cb1693773df5bee70d9324ae43465881b40ef
zhangxiaoximax <2054665596@qq.com>
2020-02-06 22:06:22 +0800

    初始化项目目录

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <test></test>
+  </div>
+</template>
+
+<script>
+import test from './components/test'
+
+export default {
+  components: {
+    test
+  }
+}
+</script>
+
+<style>
+</style>
