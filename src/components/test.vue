ce3810d5f3d24bfb2fe165eb4df40ef5db25194b
zhangxiaoximax <2054665596@qq.com>
2020-02-10 23:16:20 +0800

    完成轮播图组件

---
+++ src/components/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="test">
+    hello
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .test
+    color: red;
+</style>
