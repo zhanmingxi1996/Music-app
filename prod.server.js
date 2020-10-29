ec418396de1f73632fe4ecee1186b685bad844ec
zhangxiaoxi <2054665596@qq.com>
2020-10-29 18:04:07 +0800

    可发布版本

---
+++ prod.server.js
@@ -0,0 +1,150 @@
+const express = require('express')
+
+const app = express()
+
+const router = express.Router()
+
+router.get('/getDiscList', function (req, res) {
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+router.get('/getCdInfo', function (req, res) {
+  const url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    let ret = response.data
+    if (typeof ret === 'string') {
+      const reg = /^\w+\(({.+})\)$/
+      const matches = ret.match(reg)
+      if (matches) {
+        ret = JSON.parse(matches[1])
+      }
+    }
+    res.json(ret)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+router.get('/lyric', function (req, res) {
+  const url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    let ret = response.data
+    if (typeof ret === 'string') {
+      const reg = /^\w+\(({.+})\)$/
+      const matches = ret.match(reg)
+      if (matches) {
+        ret = JSON.parse(matches[1])
+      }
+    }
+    res.json(ret)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+router.post('/getPurlUrl', bodyParser.json(), function (req, res) {
+  const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
+  axios.post(url, req.body, {
+    headers: {
+      referer: 'https://y.qq.com/',
+      origin: 'https://y.qq.com',
+      'Content-type': 'application/x-www-form-urlencoded'
+    }
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+router.get('/search', function (req, res) {
+  const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+router.get('/getTopBanner', function (req, res) {
+  const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
+  const jumpPrefix = 'https://y.qq.com/n/yqq/album/'
+
+  axios.get(url, {
+    headers: {
+      referer: 'https://u.y.qq.com/',
+      host: 'u.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    response = response.data
+    if (response.code === 0) {
+      const slider = []
+      const content = response.focus.data && response.focus.data.content
+      if (content) {
+        for (let i = 0; i < content.length; i++) {
+          const item = content[i]
+          const sliderItem = {}
+          sliderItem.id = item.id
+          sliderItem.linkUrl = jumpPrefix + item.jump_info.url + '.html'
+          sliderItem.picUrl = item.pic_info.url
+          slider.push(sliderItem)
+        }
+      }
+      res.json({
+        code: 0,
+        data: {
+          slider
+        }
+      })
+    } else {
+      res.json(response)
+    }
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
+app.use('/api', router)
+
+app.use(express.static('./dist'))
+
+const port = process.env.PORT || 9000
+
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('Listening at http://localhost:' + port + '\n')
+})
