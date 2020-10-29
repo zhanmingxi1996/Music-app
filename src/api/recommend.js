fcd5eba7a1903b932aa155b6a6128099dae761a5
zhangxiaoxi <2054665596@qq.com>
2020-10-29 17:50:16 +0800

    finish

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -5,7 +5,7 @@ const debug = process.env.NODE_ENV !== 'production'
 
 export function getRecommend () {
   // 线上环境地址，根据实际上线配置修改
-  const url = debug ? '/api/getTopBanner' : 'http://xxx.com/music/api/getTopBanner'
+  const url = debug ? '/api/getTopBanner' : 'http://120.79.81.194/music/api/getTopBanner'
 
   const data = Object.assign({}, commonParams, {
     platform: 'yqq.json',
@@ -48,7 +48,7 @@ export function getRecommend () {
 
 export function getDiscList () {
   // 线上环境地址，根据实际上线配置修改
-  const url = debug ? '/api/getDiscList' : 'http://xxx.com/music/api/getDiscList'
+  const url = debug ? '/api/getDiscList' : 'http://120.79.81.194/music/api/getDiscList'
 
   const data = Object.assign({}, commonParams, {
     platform: 'yqq',
@@ -70,7 +70,7 @@ export function getDiscList () {
 }
 
 export function getSongList (disstid) {
-  const url = debug ? '/api/getCdInfo' : 'http://xxx.com/music/api/getCdInfo'
+  const url = debug ? '/api/getCdInfo' : 'http://120.79.81.194/music/api/getCdInfo'
 
   const data = Object.assign({}, commonParams, {
     disstid,
